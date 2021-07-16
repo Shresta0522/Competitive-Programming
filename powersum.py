744e78e4d7dcc09225b0f39a4d1b1f02bcd92c8a
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-16 11:04:56 +0530

    powersum solution

--- powersum.py
+++ powersum.py
@@ -9,11 +9,26 @@
 
 # If n is negative, return 0. Similarly, if k is negative, 
 # return 0.
-
+import pytest
 def powerSum(n, k):
     # Your code goes here...
-    return 0
-
+    s=0
+    if(n<=0) or k<=0:
+        return 0
+    else:
+        for i in range(1,n+1):
+            # print(i)
+            s=s+i**k
+        # print(s)
+        return s
 # Write your own test cases here...
+@pytest.mark.parametrize('n,k,s',[
+    (0,0,0),(-1,1,0),(1,-1,0),(2,2,5)
+])
+
+def test(n,k,s):
+    assert powerSum(n,k)==s
 
 print ("All test cases passed...")
+
+# print(powerSum(2,2))
