2fac78bdeb5e667e5098f19df4bd83e39a08bf48
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-19 09:24:07 +0530

    line intersection solution

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -5,5 +5,18 @@
 # This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
 
 def lineintersection(m1, b1, m2, b2):
+	if(m1==m2 or b1==b2):
+		return None
+	# elif(ismultiple(m1,m2) or ismultiple(m2,m1)):
+	# 	return None
+	else:
+		r=(b2-b1)/(m1-m2)
+		# print(r)
+		if(r<=1):
+			return None
+		return int(r)
 	# your code goes here
-	pass
+	# return ""
+
+
+print(lineintersection(8, 13, 4, 17))
