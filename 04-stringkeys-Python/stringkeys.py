5c4859c7baad106f39610fd86c8b47f8ddd1c77d
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-19 11:42:44 +0530

    incomplete solution

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -6,24 +6,45 @@ class HashTable(object):
     def __init__(self):
         self.table = [None]*10000
 
-    def store(self, string):
+    def store(self,string):
         """Input a string that's stored in 
         the table."""
+        r=self.calculate_hash_value(string)
+
+        if(self.table[r]!=None):
+            self.table[r]=(string)
+        else:
+            self.table[r]=[string]
+            
+        return r
+
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
-        pass
         
+    # print(store("UDACITY"))  
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        if(self.table==None):
+            return -1
+            
+        r=self.calculate_hash_value(string)
+        if(string in self.table[r]):
+            return r
+        
+        return -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        pass
+        a=string[0]
+        b=string[1]
+        # print(a,b)
+        hash_value=(ord(a)*100)+(ord(b))
+        # print((ord(a)*100)+(ord(b)))
+        return hash_value
 
 
