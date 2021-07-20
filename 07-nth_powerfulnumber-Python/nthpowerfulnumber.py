3df22591066186226892c84471de97b799e78a15
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-20 19:04:11 +0530

    nth powerful number

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -6,5 +6,51 @@
 
 
 def nthpowerfulnumber(n):
+	count=1
+	if(n==0):
+		return 1
+	else:
+		count=-1
+		i=1
+		while(n>count):
+			if(powerfulnumber(i)):
+				# print(i)
+				count+=1
+			i+=1
+		return i-1
+
+
 	# Your code goes here
-	pass
+	# return None
+
+def powerfulnumber(n):
+	for i in range(1,n+1):
+		if(n%i==0):
+			# print(i)
+			if(primenumber(i)):
+				# print(i)
+				if((n%(i**2))!=0):
+					return False
+	
+	return True
+
+def primenumber(k):
+	if(k<2):
+		return False
+	elif(k==2):
+		return True
+	elif(k%2==0):
+		return False
+	else:
+		for i in range(3,k):
+			# print(k,i)
+			if((k%i)==0):
+				return False
+			
+		return True
+
+# print(primenumber(35))
+# print(powerfulnumber(35))
+
+
+print(nthpowerfulnumber(5))
