a8520193ee5dc507b7681890138390d7ebfc4631
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-20 16:42:33 +0530

    row,col,d equal solution

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -14,5 +14,45 @@
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 def ismostlymagicsquare(a):
+	
+	d=0
+	col=0
+	for i in range(len(a)):
+		r=0
+		c=0
+		# print(a[i])
+		for j in range(len(a[i])):
+			# print("j",j)
+			r+=a[i][j]
+			# print("value",a[j][i])
+			c+=a[j][i]
+		# print("c",c)
+			if(i==j):
+				# print("d",a[j][j])
+				d+=a[j][j]
+			# print("CC",c)
+		# print("j",j)
+			
+		if(i==0):
+			row=r
+			# print("r")
+		else:
+			if(row!=r):
+				return False
+
+		# print("r",r)
+		# col=c
+		# if(col!=c):
+		# 	return False
+
+		# print("c",c)
+	# print("d",d)
+
+	if(r==c) and(r==d) :
+		return True
+	else:
+		return False
 	# Your code goes here
-	pass
+	# return a 
+
+print(ismostlymagicsquare([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
