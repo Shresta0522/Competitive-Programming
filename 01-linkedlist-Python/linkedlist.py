efecddce6c212c68eddf4bc73d2e6159b4c974f0
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-25 21:19:01 +0530

    incomplete solution

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -18,15 +18,40 @@ class LinkedList(object):
         self.head = head
         
     def append(self, new_element):
+        current=self.head
+        if(current==None):
+            current.value=new_element
+            current.next=None
+        else:
+            while(current.next!=None):
+                current=current.next
+            current.value=new_element
+            current.next=None
+                
+            
+
+
         # Your code goes here
-        pass
+        
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        count=1
+        current=self.head
+        if(position<1):
+            return None
+        else:
+            while(current!=None):
+                if(count==position):
+                    return current
+                current=current.next
+                count+=1
+            return None
+
+        
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +59,26 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
-    
-    
+        count=1
+        current=self.head
+        if(position==1):
+            new_element.next=current
+            current=new_element
+        else:
+            while(current!=None):
+                if(count==position-1):
+                    new_element.next=current.next
+                    current.next=new_element
+                current=current.next
+                count+=1
+
+
+
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
+        current=self.head
+        while(current!=None):
+            if(current.value==value):
+                
         pass
