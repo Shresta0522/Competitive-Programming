66608d5dc39e80ddbbbb4eaaeda0a65a6966bb9f
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-25 21:43:50 +0530

    mid value iteration solution

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -12,4 +12,24 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    pass
+    l=0
+    h=len(input_array)-1
+    # m=0
+    # print(l)
+    while(l<=h):
+        m=(l+h)//2
+
+        if(input_array[m]>value):
+           h=m-1
+
+        elif(input_array[m]<value):
+            l=m+1
+
+        else:
+            return m
+    return -1   
+        
+
+        
+    
+# print(binary_search([1,3,9,11,15,19,29],19))
