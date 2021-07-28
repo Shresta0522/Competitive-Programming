cc94b2c89777207ba7cec55c907f861e379923bb
Shresta0522 <shrestareddy82@msitprogram.net>
2021-07-28 06:48:13 +0530

    Linked list Node  solution

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -8,6 +8,7 @@ particular value.
 Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
+        
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -18,72 +19,58 @@ class LinkedList(object):
         self.head = head
         
     def append(self, new_element):
-        current=self.head
-        if(current==None):
-            current.value=new_element
-            
+        current = self.head
+        if self.head:
+            while (current.next!=None):
+                current = current.next
+            current.next = new_element
         else:
-            while(current.next!=None):
-                current=current.next
-            current.value=new_element
-        
-            
-
-
-        # Your code goes here
-        
+            self.head = new_element
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
-        # Your code goes here
-        count=1
-        current=self.head
-        if(position<1):
-            return None
-        else:
-            while(current!=None):
-                if(count==position):
-                    return current
-                current=current.next
-                count+=1
-            return None
-
-        
+        current = self.head
+        count = 1
+        while (current!=None):
+            if (count==position):
+                return current
+            current = current.next
+            count += 1
+        return None
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        # Your code goes here
-        count=1
-        current=self.head
+        current = self.head
+        count = 1
         if(position==1):
-            new_element.next=current
-            current=new_element
-        else:
-            while(current!=None):
-                if(count==position-1):
-                    new_element.next=current.next
-                    current.next=new_element
-                current=current.next
-                count+=1
+            new_element.next = current
+            self.head = new_element
 
-
-
+        if (position>1):
+            while (current!=None):
+                if (count==position-1):
+                    new_element.next = current.next
+                    current.next = new_element
+                    break
+                current = current.next
+                count += 1
+        
+            
     def delete(self, value):
         """Delete the first node with a given value."""
-        # Your code goes here
-        current=self.head
-
-        while(current!=None):
-            if(current.next.value==value):
-                if(current.next):
-                    current.next=current.next.next
+        current = self.head
+        previous = None
+        while (current!=None):
+            if (current.value == value):
+                if (previous):
+                    previous.next = current.next
                 else:
-                    current=current.next.next
-            # else:
+                    self.head = current.next
+            previous = current
+            current = current.next
 
-        
