2302f4bee77734f8a37852b3a8574334ab878794
Shresta0522 <shrestareddy82@msitprogram.net>
2021-08-05 00:19:35 +0530

    class object solution

--- SayHi.py
+++ SayHi.py
@@ -4,5 +4,15 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+class SayHi(object):
+    def __init__(self,name,age):
+        self.age=age
+        self.name=name
+
+    def sentence(self):
+        print(" Hi, my name is "+ self.name + " and I am " + str(self.age) +" years old!")
+
+data1=SayHi("Dot",4)
+data2=SayHi("Elf",3)
+data1.sentence()
+data1.sentence()
