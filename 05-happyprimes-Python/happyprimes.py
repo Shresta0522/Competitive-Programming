ade109aa94451eb2ed374cd049293b964765b6c9
Shresta0522 <shrestareddy82@msitprogram.net>
2021-08-05 01:16:45 +0530

    happy prime solution

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -11,4 +11,50 @@
 # functions to make nthHappyNumber(n) easier to write!
 def ishappyprimenumber(n):
     # Your code goes here
-    pass
+    if(n==1):
+        return True
+
+    if(prime(n)):
+        s=sum_digits(n)
+        if(s<9 and s==1):
+            return True
+        else:
+            while(s>9):
+                print("s",s)
+                s=sum_digits(s)
+    
+            if(s==1):
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def prime(n):
+    if(n<=1):
+        return False
+    elif(n==2 or n==3):
+        return True
+    elif(n%2==0):
+        return False
+    else:
+        for i in range(3,n):
+            if(n%i==0):
+                return False
+        return True
+
+print(prime(833))
+def sum_digits(n):
+    s=0
+    while(n>0):
+        a=n%10
+        s=s+(a**2)
+        # print("s",s)
+        n=n//10
+        # print("n",n)
+        # print(a)
+        
+    return s
+# print(sum_digits(13))
+
+print(ishappyprimenumber(383))
