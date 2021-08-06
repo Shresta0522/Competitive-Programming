681a523d5244d4597f32e70464d605e7a70af9d4
Shresta0522 <shrestareddy82@msitprogram.net>
2021-08-06 19:56:43 +0530

    sets solutions

--- set.py
+++ set.py
@@ -6,7 +6,30 @@
 # Example:
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
-
+import itertools
 def limitedPowerSet(n, k):
-    # Your code goes here...
-    pass
+    s=set()
+    d=[{}]
+    for i in range(1,n+1):
+        s.add(i)
+
+
+    for i in range(1,len(s)+1):
+        a=list(map(set,itertools.combinations(s,i)))
+        for j in range(len(a)):
+            if(len(d)!=k):
+                d.append(a[j])
+            else:
+                return d
+    # if(n==k+1):
+    # for i in range(n):
+    #     if(len(d)==k):
+    #         return d
+    #     d.append({i+1})
+    # # Your code goes here...
+    # print(d)
+    # return d
+    # else:
+
+    # for i in range(d):
+print(limitedPowerSet(5,7))
